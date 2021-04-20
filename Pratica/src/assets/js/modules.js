361a68386995e43a1f0a486a45ec76dee79f6a60
matheusrcruz <git.dev@outlook.com>
2021-04-20 18:12:41 -0300

    Progress: em produção

---
+++ Pratica/src/assets/js/modules.js
@@ -0,0 +1,17 @@
+//var  variaveis de escopo global 
+//let varaveis de escpo local 
+// const global []
+/*
+por tag: getElementByTagName()
+por id: getElementById()
+por nome: getElementsByName()
+por classe: getElementsByClasseName()
+por seletor: querySelector(pratica mais recente)
+*/
+
+let nome = window.document.getElementById('nome')
+let email = document.querySelector('#email')
+let assunto = document.querySelector('#assunto')
+
+
+nome.style.width = '100%'
